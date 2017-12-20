d452d7b4367ebd977343ab89b870a6279fb11fca
SunwelLight <sunwellight@gmail.com>
2017-12-20 23:19:23 +0600

    Update CleanVendorsController.php
    deleting unnecessary messages

--- CleanVendorsController.php
+++ CleanVendorsController.php
@@ -116,20 +116,15 @@ class CleanVendorsController extends Controller
         $namePackage = $package;
 
         if(preg_match('/bower-asset/i', $package)) {
-            $this->echo_msg($package );
             $namePackage = str_replace("bower-asset", 'bower', $package);
-            $this->echo_msg($package);
         }
 
         if(preg_match('/npm-asset/i', $package)) {
-            $this->echo_msg($package );
             $namePackage = str_replace("npm-asset", 'npm', $package);
-            $this->echo_msg($package);
         }
 
         $dir = $this->vendor_dir . '/' . $namePackage;
-
-        $dir = $this->vendor_dir . '/' . $package;
+        
         if (!is_dir($dir)) {
             return false;
         }
